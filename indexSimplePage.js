a766439abd6bbb5d9bde9943e0ebdb4fd3c2ce7a
sarwan4553 <sarwankumar.2911@gmail.com>
2025-04-21 11:54:03 +0530

    initial commit

---
+++ indexSimplePage.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+//get provide the routes
+app.get("", (req, res) => {
+  // ex.:-http://localhost:5000/?name=sarwan
+  let name = req.query.name;
+  // you want to show any data help of send function
+  res.send(`<h1>hello ${name}. this is my express home page </h1>
+        <a href="/">go to about home</a> <br/>
+        <a href="/about">go to about page</a><br/>
+        <a href="/help">go to about help</a><br/>
+        <a href="/json">go to about json</a><br/>
+        
+                `);
+});
+app.get("/about", (req, res) => {
+  res.send(`
+        <h1>hello this is my express about page</h1>
+        <a href="/">go to about home</a> <br/>
+        <a href="/about">go to about page</a><br/>
+        <a href="/help">go to about help</a><br/>
+        <a href="/json">go to about json</a><br/>
+        
+        `);
+});
+app.get("/help", (req, res) => {
+  res.send(`
+        <h1>hello this is my express help page</h1>
+      <a href="/">go to about home</a> <br/>
+        <a href="/about">go to about page</a><br/>
+        <a href="/help">go to about help</a><br/>
+        <a href="/json">go to about json</a><br/>
+        
+        `);
+});
+app.get("/json", (req, res) => {
+  res.send([
+    { name: "sarwan", lastname: "kumar", email: "sarwan@yahoo.com" },
+    { name: "tapes", lastname: "saxena", email: "tapes@yahoo.com" },
+  ]);
+});
+app.listen(5000); //run your server
---
+++ index_routing.js
@@ -0,0 +1,30 @@
+// routing remove the extention of the your file in url
+const express=require('express');
+const app=express();
+// need current application patyh
+const path=require('path');
+const publicPath=path.join(__dirname,'public')
+// console.log(publicPath);
+/**
+ * app= this app use express exicution function
+ * express.static=use in static funciton in express
+ */
+// use this statis path
+// app.use(express.static(publicPath))
+/**
+ * static data routing
+ */
+app.get('', (req,res)=>{
+    res.sendFile(`${publicPath}/index.html`)
+})
+app.get('/aboutme', (req,res)=>{
+    res.sendFile(`${publicPath}/about.html`)
+})
+app.get('/helpme', (req,res)=>{
+    res.sendFile(`${publicPath}/help.html`)
+})
+// app.get('*',(_,resp)=>{
+//     resp.sendFile()
+// })
+
+app.listen(5000)
